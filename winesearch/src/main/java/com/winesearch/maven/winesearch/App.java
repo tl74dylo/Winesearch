f9fc7cf0440d0bd025b080d5dc7e9702b3cb06ca
martinoz1024 <martinoz.lorenz@gmail.com>
2019-06-09 16:01:42 +0200

    Demo as Maven Project with Test Data

---
+++ winesearch/src/main/java/com/winesearch/maven/winesearch/App.java
@@ -0,0 +1,20 @@
+package com.winesearch.maven.winesearch;
+
+import com.opencsv.CSVReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
