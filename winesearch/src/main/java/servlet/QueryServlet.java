dd19ea01c99ade934455aea08734751d59b3ca42
Monster Hunter <50144238+tl74dylo@users.noreply.github.com>
2019-08-13 17:31:10 +0200

    Queryverarbeitung überarbeitet
    jetzt kann nach german wine etc gesucht werden

--- winesearch/src/main/java/servlet/QueryServlet.java
+++ winesearch/src/main/java/servlet/QueryServlet.java
@@ -51,7 +51,7 @@ import javax.servlet.http.HttpServletResponse;
 public class QueryServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private int numOfResults = 0;
-
+	private int best = 0;
     /**
      * Default constructor. 
      */
@@ -64,6 +64,7 @@ public class QueryServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String query = request.getParameter("query").toLowerCase();
+		String queryneu = null;
 		String evalMode = request.getParameter("eval");
 		boolean eval = ((evalMode!= null) ? true: false);
 
@@ -71,7 +72,12 @@ public class QueryServlet extends HttpServlet {
 		//PrintWriter writer = response.getWriter();
 		logge(query);
 		System.out.println("geloggt");
-		String queryneu = check(query);
+		if (eval == true) {
+		queryneu = check(query);
+		} else {
+			queryneu = query;
+		}
+		System.out.println("query: "+query);
 		 ArrayList<Document> resultsList = new ArrayList<Document>();
 		try {
 			//resultsList = searchIndexedFiles(writer, query);
@@ -82,6 +88,9 @@ public class QueryServlet extends HttpServlet {
 		}
 		
 		Map <String, Integer> map = setupRequest(request, query, eval);
+		if(best == 1 && eval == true) {
+			sortbest(resultsList);
+		}
 		sort(resultsList, map);
 		request.setAttribute("results", resultsList);
 		request.getRequestDispatcher("results.jsp").forward(request, response);
@@ -236,66 +245,83 @@ public class QueryServlet extends HttpServlet {
 		}
 	}
 	
-	private String check(String query) {	//Vorverarbeitungsfunktion fuer Queries
-		StringBuilder sb = new StringBuilder();
-		String [] queryarr = query.split(" ");	//splittet die Query
+	private String check(String query) {	//Vorverarbeitungsfunktion fuer Queries		
+		best = 0;
+		String queryneu = checkvintage(query);
+		queryneu = checktype(queryneu);
+		queryneu = checkbest(queryneu);
+		queryneu = checkcountry(queryneu);
 		
+		System.out.println(query +" (original)");	
+		System.out.println(queryneu+" (neu)");
+		return queryneu;
 		
+	}
 		//checks if vintage is requested
-		int vintage = 0;					//Kenngroesse fuer doppelvorkommen von woertern
-			if (queryarr.length>1) {		//iteriert über jedes Wort der Query, falls query laenger als 1 (da Vintage ein Jahr braucht)
-				for (int j = 0; j<queryarr.length; j++) {
-				/*if (queryarr[queryarr.length-1].equals("vintage") && vintage == 0){	//check ob das letzte Wort == vintage und Kenngroesse noch 0
-					for (int k = 0; k<(queryarr.length-1); k++) {
+	private String checkvintage(String query) {
+		String [] queryarr = query.split(" ");
+		StringBuilder sb = new StringBuilder();
+			int vintage = 0;					//Kenngroesse fuer doppelvorkommen von woertern
+				if (queryarr.length>1) {		//iteriert über jedes Wort der Query, falls query laenger als 1 (da Vintage ein Jahr braucht)
+					for (int j = 0; j<queryarr.length; j++) {
+					/*if (queryarr[queryarr.length-1].equals("vintage") && vintage == 0){	//check ob das letzte Wort == vintage und Kenngroesse noch 0
+						for (int k = 0; k<(queryarr.length-1); k++) {
 						
-						try {
-							int zahl = Integer.parseInt(queryarr[k]);	//falls wort == zahl, erfolg
-							if (zahl > 1900 && zahl < 2100) {	
-							sb.append("title:");						//falls im Zeitraum, neue Query einfuegen
-							sb.append(queryarr[k]+" ");
+							try {
+								int zahl = Integer.parseInt(queryarr[k]);	//falls wort == zahl, erfolg
+								if (zahl > 1900 && zahl < 2100) {	
+								sb.append("title:");						//falls im Zeitraum, neue Query einfuegen
+								sb.append(queryarr[k]+" ");
 							}
-						} catch (NumberFormatException nfe) {			//falls keine Zahl, wort zur neuen Query hinzufuegen
-							sb.append(queryarr[k]+" ");
-						}
-						
-					}
-					vintage = 1;
-				} else */ if (vintage != 1 && queryarr[j].equals("vintage")) {
-					for (int l = 0; l<j; l++) {
-						try {
-							int zahl = Integer.parseInt(queryarr[l]);
-							if (zahl > 1900 && zahl < 2100) {
-							sb.append("title:");
-							sb.append(queryarr[l]+" ");
+							} catch (NumberFormatException nfe) {			//falls keine Zahl, wort zur neuen Query hinzufuegen
+								sb.append(queryarr[k]+" ");
 							}
-						} catch (NumberFormatException nfe) {
-							sb.append(queryarr[l]+" ");
+								
 						}
-					}
-					sb.append("vintage ");
-					for(int l = j+1; l<queryarr.length; l++) {
-						try {
-							int zahl = Integer.parseInt(queryarr[l]);
-							if (zahl > 1900 && zahl < 2100) {
+						vintage = 1;
+					} else */ if (vintage != 1 && queryarr[j].equals("vintage")) {
+						for (int l = 0; l<j; l++) {
+							try {
+								int zahl = Integer.parseInt(queryarr[l]);
+								if (zahl > 1900 && zahl < 2100) {
 								sb.append("title:");
 								sb.append(queryarr[l]+" ");
 								}
-						} catch (NumberFormatException nfe) {
-							sb.append(queryarr[l]+" ");
+							} catch (NumberFormatException nfe) {
+								sb.append(queryarr[l]+" ");
+							}
 						}
+						sb.append("vintage ");
+						for(int l = j+1; l<queryarr.length; l++) {
+							try {
+								int zahl = Integer.parseInt(queryarr[l]);
+								if (zahl > 1900 && zahl < 2100) {
+									sb.append("title:");
+									sb.append(queryarr[l]+" ");
+									}
+							} catch (NumberFormatException nfe) {
+								sb.append(queryarr[l]+" ");
+							}
+						}
+						vintage = 1;
+						 }
+					
 					}
-					vintage = 1;
-				  }
-				
+						
+				} 
+				if (sb.length() == 0){
+				sb.append(query);
 				}
-				
-			} 
-			if (sb.length() == 0){
-			sb.append(query);
-			}
+				return sb.toString();
+	}
+		
 		
 		//checks if only red or white wine is requested
-				int type = 0;
+	private String checktype(String query) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(query);
+		String [] queryarr = query.split(" ");
+		int type = 0;
 		for (int i=0; i<queryarr.length; i++) {
 			if (queryarr[i].equals("red") || queryarr[i].equals("rouge")) {
 				if (type != 1) {
@@ -306,8 +332,8 @@ public class QueryServlet extends HttpServlet {
 					type += 2;
 				}
 			}
+			
 		}
-		
 		
 		if (type == 1) {
 			sb.append(" -blanc");
@@ -317,11 +343,180 @@ public class QueryServlet extends HttpServlet {
 			sb.append(" -rouge");
 		}
 		
-		
-		
-		System.out.println(query +" (original)");
 		String queryneu = sb.toString();
-		System.out.println(queryneu+" (neu)");
+		return queryneu;
+	}
+		
+	
+	//country check, doesn't check every country, but a fair amount
+		
+	private String checkcountry(String query) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(query);
+		String queryarr[] = query.split(" ");
+		for (int i=0; i<queryarr.length; i++) {
+			switch(queryarr[i]) {
+				case "france":
+					sb.append("+country:france");
+					break;
+				case "french":
+					sb.append("+country:france");
+					break;
+				case "germany":
+					sb.append("+country:germany");
+					break;
+				case "german":
+					sb.append("+country:germany");
+					break;
+				case "america":
+					sb.append("+country:us");
+					break;
+				case "american":
+					sb.append("+country:us");
+					break;
+				case "us":
+					sb.append("+country:us");
+					break;
+				case "italy":
+					sb.append("+country:italy");
+					break;
+				case "italian":
+					sb.append("+country:italy");
+					break;
+				case "greece":
+					sb.append("+country:greece");
+					break;
+				case "greek":
+					sb.append("+country:greece");
+					break;
+				case "spain":
+					sb.append("+country:spain");
+					break;
+				case "spanish":
+					sb.append("+country:spain");
+					break;
+				case "mexico":
+					sb.append("+country:mexico");
+					break;
+				case "mexican":
+					sb.append("+country:mexico");
+					break;
+				case "australia":
+					sb.append("+country:australia");
+					break;
+				case "australian":
+					sb.append("+country:australia");
+					break;
+				case "austria":
+					sb.append("+country:austria");
+					break;
+				case "austrian":
+					sb.append("+country:austria");
+					break;
+				case "portugal":
+					sb.append("+country:portugal");
+					break;
+				case "portuguese":
+					sb.append("+country:portugal");
+					break;
+				case "argentina":
+					sb.append("+country:argentina");
+					break;
+				case "argentinean":
+					sb.append("+country:argentina");
+					break;
+				case "romania":
+					sb.append("+country:romania");
+					break;
+				case "romanian":
+					sb.append("+country:romania");
+					break;
+				case "england":
+					sb.append("+country:england");
+					break;
+				case "english":
+					sb.append("+country:england");
+					break;
+				case "georgia":
+					sb.append("+country:georgia");
+					break;
+				case "georgian":
+					sb.append("+country:georgia");
+					break;
+				case "canada":
+					sb.append("+country:canada");
+					break;
+				case "canadian":
+					sb.append("+country:canada");
+					break;
+				case "zealand":
+					sb.append("+country:zealand");
+					break;
+				case "africa":
+					sb.append("+country:africa");
+					break;
+				case "african":
+					sb.append("+country:africa");
+					break;
+				case "israel":
+					sb.append("+country:israel");
+					break;
+				case "chile":
+					sb.append("+country:chile");
+					break;
+				case "chilean":
+					sb.append("+country:chile");
+					break;
+				case "bulgaria":
+					sb.append("+country:bulgaria");
+					break;
+				case "bulgarian":
+					sb.append("+country:bulgaria");
+					break;
+				case "moldova":
+					sb.append("+country:moldova");
+					break;
+				case "moldavian":
+					sb.append("+country:moldova");
+					break;
+				case "slovenia":
+					sb.append("+country:slovenia");
+					break;
+				case "slovenian":
+					sb.append("+country:slovenia");
+					break;
+				case "hungary":
+					sb.append("+country:hungary");
+					break;
+				case "hungarian":
+					sb.append("+country:hungary");
+					break;
+				case "cyprus":
+					sb.append("+country:cyprus");
+					break;
+				default:
+					break;
+			}
+				
+		}
+		
+		return sb.toString();
+	}
+		
+		
+		//best
+	
+	private String checkbest (String query) {
+		StringBuilder sb = new StringBuilder();
+		String queryarr[] = query.split(" ");
+		for (int i=0; i<queryarr.length; i++) {
+			if (queryarr[i].equals("best")) {
+			best = 1;
+		} else {
+			sb.append(queryarr[i]+" ");
+			}
+		}	
+		String queryneu = sb.toString();
 		return queryneu;
 	}
 	
@@ -331,7 +526,7 @@ public class QueryServlet extends HttpServlet {
 		do {
 			tausch = 0;
 		try {
-		for (int i= 0; i<=50;i++){
+		for (int i= 0; i<=100;i++){
 		if (hashmap.get(resultList.get(i).get("id")) < hashmap.get(resultList.get(i+1).get("id"))) {
 			Document temp = resultList.get(i);
 			resultList.set(i, resultList.get(i+1));
@@ -346,6 +541,31 @@ public class QueryServlet extends HttpServlet {
 		}
 		} while (tausch == 1);
 	}
+	
+	private void sortbest(ArrayList<Document> resultList) {
+		int tausch = 0;
+		do {
+			 tausch = 0;
+		try {
+			for (int i= 0; i<=100;i++){
+				int a = Integer.parseInt(resultList.get(i).get("points"));
+				int b = Integer.parseInt(resultList.get(i+1).get("points"));
+				if(a<b) {
+					Document temp = resultList.get(i);
+					resultList.set(i, resultList.get(i+1));
+					resultList.set((i+1), temp);
+					tausch = 1;
+						}
+				}
+			
+		} catch (IndexOutOfBoundsException ioe) {
+			
+		} catch (NullPointerException npe) {
+			
+		;}
+	} while (tausch == 1);
+	
+	}  
 	  
 
 }
